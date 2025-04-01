9c43ea178129e58d2294abe65bd293a08f52f172
Haritiana-PC <haritiana@kali>
2025-04-02 00:38:41 +0300

    Ajout de paragraphe

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
       <div>
         <h1>Zioty</h1>
+        <p>Crée votre site tout simplement</p>
       </div>
     </>
   )
